2bf2b7626ce46da92381cb472fd93d3d80be947b
LightMingMing <1849901944@qq.com>
2019-03-26 22:57:08 +0800

    Add read and cancel operations

--- src/main/java/nio/samples/SelectorSample.java
+++ src/main/java/nio/samples/SelectorSample.java
@@ -2,38 +2,73 @@ package nio.samples;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-public class SelectorSample {
+/**
+ * Selector sample: multi channels select
+ *
+ * @author LightMingMing
+ */
+public class SelectorSample extends Thread {
 
-    private static Selector selector;
+    private Selector selector;
+    private ServerSocketChannel[] serverSocketChannels;
 
-    static {
+    private AtomicBoolean shutdown = new AtomicBoolean();
+
+    public SelectorSample(int... ports) {
         try {
             selector = Selector.open();
+            serverSocketChannels = new ServerSocketChannel[ports.length];
+            for (int i = 0; i < ports.length; i++) {
+                serverSocketChannels[i] = ServerSocketChannel.open();
+                serverSocketChannels[i].configureBlocking(false);
+                serverSocketChannels[i].bind(new InetSocketAddress(ports[i]));
+                serverSocketChannels[i].register(selector, SelectionKey.OP_ACCEPT);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        int[] ports = {8080, 8081, 8082, 8083, 8084};
-        for (int port : ports) {
-            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-            serverSocketChannel.configureBlocking(false);
-            serverSocketChannel.bind(new InetSocketAddress(port));
-            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-            System.out.println(port);
-        }
-        while (true) {
-            System.out.println(selector.select(SelectorSample::consume));
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println(new Date() + "Starting");
+        SelectorSample task = new SelectorSample(8080, 8081, 8082);
+        task.start();
+        TimeUnit.SECONDS.sleep(60);
+        task.cancel();
+        task.join(1000);
+        System.out.println(new Date() + " End");
+    }
+
+    @Override
+    public void run() {
+        while (!shutdown.get()) {
+            try {
+                System.out.println("keys number: " + selector.select(this::accept));
+            } catch (IOException e) {
+                e.printStackTrace();
+                shutdown.set(true);
+            }
         }
     }
 
-    private static void consume(SelectionKey selectedKey) {
+    public void cancel() throws IOException {
+        shutdown.set(true);
+        for (ServerSocketChannel serverSocketChannel : serverSocketChannels) {
+            serverSocketChannel.close();
+        }
+        this.selector.close();
+    }
+
+    private void accept(SelectionKey selectedKey) {
         try {
             if (selectedKey.isAcceptable()) {
                 ServerSocketChannel serverSocketChannel = (ServerSocketChannel) selectedKey.channel();
@@ -41,12 +76,32 @@ public class SelectorSample {
                 socketChannel.configureBlocking(false);
                 socketChannel.register(selector, SelectionKey.OP_READ);
                 System.out.println(socketChannel.getLocalAddress() + " " + socketChannel.getRemoteAddress());
+            } else if (selectedKey.isReadable()) {
+                read(selectedKey);
             } else {
                 selectedKey.cancel();
                 selectedKey.channel().close();
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void read(SelectionKey selectedKey) {
+        SocketChannel socketChannel = (SocketChannel) selectedKey.channel();
+        final int maxReadLength = 1024;
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(maxReadLength);
+        try {
+            int readBytes = socketChannel.read(byteBuffer);
+            System.out.println("read bytes: " + readBytes);
+            if (readBytes == -1)
+                socketChannel.close();
+            byteBuffer.flip();
+            socketChannel.write(byteBuffer);
+            byteBuffer.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
+            selectedKey.cancel();
         }
     }
 }
